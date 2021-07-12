ab3e4e35fb85a77ab363046db9f66cc0fb70abf8
ding-co <80014673+ding-co@users.noreply.github.com>
2021-07-12 02:26:01 +0000

    Add DFS example

---
+++ stack&queue/dfs&bfs/dfs_example.py
@@ -0,0 +1,32 @@
+# DFS (Depth First Search) example 01
+
+def dfs(graph, start_node, visited):
+
+  # Handle current node visit
+  visited[start_node] = True
+  print(start_node, end=' ')
+
+  # Recursive visit other node which is connected to current node
+  for i in graph[start_node]:
+    if not visited[i]:
+      dfs(graph, i, visited)
+
+# Representation of each node's connection (2 Dimension list)
+graph = [
+  [],
+  [2, 3, 8],
+  [1, 7],
+  [1, 4, 5],
+  [3, 5],
+  [3, 4],
+  [7],
+  [2, 6, 8],
+  [1, 7]
+]
+
+# Represent each node's visiting information (1 dimension list)
+visited = [False] * 9
+
+# Call defined DFS function
+dfs(graph, 1, visited)
+print()
