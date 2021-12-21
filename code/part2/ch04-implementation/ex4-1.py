0be8091a36b4e8d3583251725bc372bd70875811
ding-co <pangoons@naver.com>
2021-12-21 09:27:48 +0900

    Add Part 2 - Implementation 01

---
+++ code/part2/ch04-implementation/ex4-1.py
@@ -0,0 +1,21 @@
+# 상하좌우
+# 풀이 시간 15분, 시간 제한 1초, 메모리 제한 128MB
+
+n = int(input())
+plans = input().split()
+
+x, y = 1, 1
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+move_types = ['L', 'R', 'U', 'D']
+
+for plan in plans:
+  for i in range(len(move_types)):
+    if plan == move_types[i]:
+      nx = x + dx[i]
+      ny = y + dy[i]
+  if nx < 1 or ny < 1 or nx > n or ny > n:
+    continue
+  x, y = nx, ny
+
+print(x, y)
